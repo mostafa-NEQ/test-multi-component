242afaef5dd51368ec5ac3db640a7929764ddd2d
Mostafa Neqrouz <mostafa.neqrouz.fycomputing@gmail.com>
2026-03-11 16:10:37 +0000

    Update components to return ComponentOutput

--- multi_package/component_two/src/component.py
+++ multi_package/component_two/src/component.py
@@ -1,6 +1,8 @@
+from iflow_engine_sdk.interfaces.output import ComponentOutput
+
 class ComponentTwoComponent:
     def __init__(self, config):
         self.config = config
 
     def process(self, text=None, **kwargs):
-        return {"result": f"Component Two processed: {text}"}
+        return ComponentOutput(result={'result': f'Component Two processed: {text}'})
